7b13bbfbf1082b6870296316555bcf7510c76b70
almaarri <alex.kovaka@gmail.com>
2020-10-31 06:45:55 -0400

    working etch a sketch code

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -2,18 +2,31 @@
 //select container
 const container = document.querySelector('#container');
 
+//create array to hold divs
+let divs = [];
+
+//create counter for array index
+let counter = 0;
+
 //create divs
 function createDivs(){
 for(i=1;i<17;i++){
     for (j=1; j<17;j++) {
-    //let divname = `divcolumn${i}row${j}`;
-    const `divcolumn${i}row${j}` = document.createElement('div');
-    //content.classList.add('divname');
-    divname.style.gridColumn = i;
-    divname.style.gridRow = j;
-    container.appendChild(divname);
+    divs[counter] = document.createElement('div');
+    divs[counter].classList.add('gridDivs');
+    divs[counter].style.gridColumn = i;
+    divs[counter].style.gridRow = j;
+    container.appendChild(divs[counter]);
+    counter++;
     }//j for
 }//i for
+
+//add even listener
+let divList = document.querySelectorAll(".gridDivs");
+divList.forEach(function(divs)
+{ divs.addEventListener('mouseenter', function(){divs.style.backgroundColor = 'black';})});
+//divObject.addEventListener('mouseenter', function(){divObject.style.backgroundColor = 'black';});
+
 }//createDivs
 
 createDivs();
