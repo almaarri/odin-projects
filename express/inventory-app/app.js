73c076b6fc388a66d66f8fd314cf9eab052decdb
almaarri <alex.kovaka@gmail.com>
2020-11-21 08:48:23 -0500

    security fix, begin new project inventory-app

--- express/inventory-app/app.js
+++ express/inventory-app/app.js
@@ -3,28 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog'); 
 
 var app = express();
 
-//Set up mongoose connection
+//set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI1;
+var mongoDB = process.env.MONGODB_URI2;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-//bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
